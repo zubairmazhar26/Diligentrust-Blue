e74b72537dc4dbb92e9c5eca736d2cc4fb3c0433
zubair <zubairmazhar26@gmail.com>
2022-11-09 10:44:24 +0000

    website setting error solve

---
+++ comp_diligentrust/company_diligentrust/doctype/whitelabel_setting/test_whitelabel_setting.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Muhammad Zubair form Diligentrust and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestWhitelabelSetting(unittest.TestCase):
+	pass
---
+++ comp_diligentrust/company_diligentrust/doctype/whitelabel_setting/whitelabel_setting.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Muhammad Zubair form Diligentrust and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Whitelabel Setting', {
+	// refresh: function(frm) {
+
+	// }
+});
